9539fd93fb2f8b91c80305cf1914fc5881c74384
Matt Carroll <oholiab@gmail.com>
2012-09-07 16:13:45 +0100

    Made everything better
    Stuff in /views/pages automagically routes and populates the nav bar,
    404s now work, pages variable is worked out once at app instantiation
    and not again after making it go moar better

--- app.rb
+++ app.rb
@@ -3,6 +3,28 @@
 require 'sinatra'
 require 'haml'
 
+def getpages
+  pages = Dir.entries([File.dirname(__FILE__),"views/pages"].join("/"))
+  pagenames = []
+  pages.each {|x| pagenames.push(x.split(".")[0])}
+  pagenames
+end
+  
+$pages = getpages
+
 get '/' do
   haml :default, :locals => {:foo => "dicks"}
+end
+
+get %r{/([\w]+)} do |word|
+  pages = getpages
+  if pages.include?(word)
+    haml ["pages",word].join("/").to_sym
+  else
+    404
+  end
+end
+
+error 404 do
+  haml :'404'
 end
